2d85967629621feae82e7328dbcba38210689d5f
ChamodKanishka <chamodkanishka77@gmail.com>
2020-02-21 12:21:25 +0530

    add a main button to home page

--- Screen/Home.js
+++ Screen/Home.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, ImageBackground, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
-
+import {StyleSheet, Text, View, ImageBackground, SafeAreaView, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+import CircleButton from 'react-native-circle-button';
+import {Header, Icon, Left, Right} from "native-base";
 
 const { width: WIDTH } = Dimensions.get('window')
 class Home extends Component {
     render() {
         return (
-            <Text>Home</Text>
+            <View style={styles.container}>
+                <Header style={styles.header}>
+                    <Left>
+                        <Icon name="menu"/>
+                    </Left>
+                    <Right>
+                        <Text style={styles.right}>Transport Service</Text>
+                    </Right>
+                </Header>
+                    <CircleButton size={100} />
+            </View>
         );
     }
 }
 export default Home;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    },
+    header:{
+        backgroundColor:"#2196f3"
+    },
+    right:{
+        fontSize:24,
+        color:"white"
+    }
+})
