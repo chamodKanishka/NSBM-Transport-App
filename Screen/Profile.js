9dac30f260ddb0997a84547fc3ae840c3b3d6bf1
ChamodKanishka <chamodkanishka77@gmail.com>
2020-02-21 22:09:39 +0530

    add back buttons for secondary pages headers

--- Screen/Profile.js
+++ Screen/Profile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View, ImageBackground, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
 import {Header, Icon, Left, Right} from "native-base";
+import Icons from 'react-native-vector-icons/Ionicons';
+import Home from './Home';
 
 
 const { width: WIDTH } = Dimensions.get('window')
@@ -8,12 +10,13 @@ class Profile extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Header style={styles.header}>
+                <Header style={{backgroundColor:"#2196f3"}}>
                     <Left>
-                        <Icon name="menu"/>
+                        <Icons name="md-arrow-back" size={30} onPress={() =>this.props.navigation.navigate("Home", {screen:Home})}/>
+
                     </Left>
                     <Right>
-                        <Text style={styles.right}>Profile</Text>
+                        <Text style={{fontSize:24, color:"white"}}>Profile</Text>
                     </Right>
                 </Header>
             </View>
