0743555d64811e7cfbfee6fd1076ecac5b224fdd
ChamodKanishka <chamodkanishka77@gmail.com>
2020-02-22 22:24:31 +0530

    add settings pages basic components

--- Screen/Settings.js
+++ Screen/Settings.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import {StyleSheet, Text, View, ImageBackground, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
 import {Header, Icon, Left, Right} from "native-base";
 import Icons from 'react-native-vector-icons/Ionicons';
+import ReactNativeSettingsPage, {
+    SectionRow,
+    NavigateRow,
+    CheckRow,
+    SwitchRow,
+    SliderRow
+} from 'react-native-settings-page';
 
 import Home from './Home';
 
@@ -20,6 +27,30 @@ class Settings extends Component {
                         <Text style={{fontSize:24, color:"white"}}>Settings</Text>
                     </Right>
                 </Header>
+                <ReactNativeSettingsPage>
+                    <SectionRow text='Settings'>
+                        <NavigateRow
+                            text='Profile'
+                            iconName='ios-contact'
+                            />
+                        <SwitchRow
+                            text='Allow Push Notification'
+                            iconName='your-icon-name'
+                           />
+                        <CheckRow
+                            text='Hide My Details'
+                            iconName='your-icon-name'
+                            _color='#000'
+                            />
+                        <SliderRow
+                            text='Slider Row'
+                            iconName='your-icon-name'
+                            _color='#000'
+                            _min={0}
+                            _max={100}
+                            />
+                    </SectionRow>
+                </ReactNativeSettingsPage>
             </View>
         );
     }
