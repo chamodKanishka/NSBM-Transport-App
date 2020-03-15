44cd86f192991426b1afd7ce94cc8521298f1c9d
ChamodKanishka <chamodkanishka77@gmail.com>
2020-03-15 13:51:14 +0530

    add maps and request to gps enable

--- Screen/Location.js
+++ Screen/Location.js
@@ -8,17 +8,7 @@ import LocationServicesDialogBox from "react-native-android-location-services-di
 
 
 
-const origin = {latitude: 6.8211, longitude: 80.0409};
-const waypoints ={latitude:6.9021, longitude:79.9195};
-const destination = {latitude: 6.9093, longitude: 79.8967};
-const mode = "DRIVING";
-const GOOGLE_MAPS_APIKEY = 'AIzaSyDPSp37X3waDDvB0X6-GXHOJdnREHsJHyY';
-
-class Location extends Component {  
-    componentWillUnmount() {
-        // used only when "providerListener" is enabled
-        LocationServicesDialogBox.stopListener(); // Stop the "locationProviderStatusChange" listener
-    }
+class Location extends Component {
     render() {
         LocationServicesDialogBox.checkLocationServicesIsEnabled({
             message: "<h2 style='color: #0af13e'>Use Location ?</h2>This app wants to change your device settings:<br/><br/>Use GPS, Wi-Fi, and cell network for location<br/><br/><a href='#'>Learn more</a>",
