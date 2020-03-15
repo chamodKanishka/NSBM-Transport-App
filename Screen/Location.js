6bc9aa10f6e1715ffbd0afc0c0fa98d07ddaacf4
ChamodKanishka <chamodkanishka77@gmail.com>
2020-03-15 09:11:32 +0530

    add maps to location page and it checks. also updated google-maps and google-base versions to newest versions

--- Screen/Location.js
+++ Screen/Location.js
@@ -3,22 +3,24 @@ import {StyleSheet, Text, View, ImageBackground, Image, TextInput, Dimensions, T
 import {Header, Icon, Left, Right} from "native-base";
 import  Icons  from 'react-native-vector-icons/Ionicons'
 import Home from './Home';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 
-const { width: WIDTH } = Dimensions.get('window')
 class Location extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Header style={{backgroundColor:"#2196f3"}}>
-                    <Left>
-                        <Icons name="md-arrow-back" size={30} onPress={() =>this.props.navigation.navigate("Home", {screen:Home})}/>
-
-                    </Left>
-                    <Right>
-                        <Text style={{fontSize:24, color:"white"}}>Location</Text>
-                    </Right>
-                </Header>
+                <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+                >
+                </MapView>
             </View>
         );
     }
@@ -26,14 +28,15 @@ class Location extends Component {
 export default Location;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
     },
-    header:{
-        backgroundColor:"#2196f3"
+    map: {
+      ...StyleSheet.absoluteFillObject,
     },
-    right:{
-        fontSize:24,
-        color:"white"
-    }
-});
+   });
+   
